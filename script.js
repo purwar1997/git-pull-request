05d99a76109592942d1dab8b9ffa88d88b636a71
shubam-work05 <shubhampurwar97@gmail.com>
2025-02-14 11:30:36 +0530

    Change text when a button is clicked

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const clickBtn = document.querySelector('.click-btn');
+const paragraph = document.querySelector('.text');
 
 clickBtn.addEventListener('click', function () {
+  paragraph.textContent = 'Pull requests are used by developers to collaborate.';
   console.log('Button clicked');
 });
